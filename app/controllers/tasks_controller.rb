29228f3d4d188ab2d5c7a5700cc84596ffbb5e44
sushiguruma <sushiguruma.honeycomb321@gmail.com>
2018-11-02 14:34:25 +0000

    CRUD complete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,57 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @tasks = Task.find(params[:id])
+  end
+  
+  def new
+    @tasks = Task.new
+  end
+  
+  def create
+    @tasks = Task.new(task_params)
+    
+    if @tasks.save
+      flash[:success] = 'タスクの追加が完了しました。'
+      redirect_to tasks_url
+    else
+      flash.now[:danger] = 'タスクの追加に失敗しました'
+      render :new
+    end
+  end
+  
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+  
+  def update
+    @tasks = Task.find(params[:id])
+    
+    if @tasks.save
+      flash[:success] = 'タスクの変更が完了しました'
+      redirect_to @tasks
+    else
+      flash[:danger] = 'タスクの変更に失敗しました'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'タスクを消去しました。'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+end
