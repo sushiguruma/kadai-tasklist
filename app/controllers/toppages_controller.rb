094530bfb15bb9e27fce419ed3713471fe765249
sushiguruma <sushiguruma.honeycomb321@gmail.com>
2018-11-17 10:59:28 +0000

    minor update

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     if logged_in?
-      @tasks_not_done = current_user.tasks.where(status: '未実行').order('created_at DESC').page(params[:page]).per(5)
+      @tasks = current_user.tasks.order('status DESC').order('created_at DESC').page(params[:page]).per(5)
     end
   end
 end
